dde9cfa8982bf246397fe0dc4c4bfb9619d68e52
Sakhumzi Kwaza <sakhumzikwaza220@gmail.com>
2025-03-05 13:03:12 +0200

    Add parsing demo and update project files
    Replaced "Hello, World!" in `Program.cs` with a demo for parsing numerical data using `int.TryParse` and `float.TryParse`. Added and updated various project files including dependencies, runtime configurations, and build settings for .NET 9.0. Included compiled binaries and debugging symbols for the `PassingNumbers` project.

--- C# and Dotnet Training/PassingNumbers/Program.cs
+++ C# and Dotnet Training/PassingNumbers/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Parsing numerical data from strings
+
+using System.ComponentModel;
+
+string[] NumStr = { "  1 ", " 1.45  ", "-100", "5e+04 " };
+
+int testint;
+float testfloat;
+bool result;
+
+//throws an exception if the parse unsuccessful
+//foreach (string str in NumStr)
+//{
+//    try
+//    {
+//        testfloat = float.Parse(str);
+//        Console.WriteLine($"Parsed number is {testfloat}");
+//        testint = int.Parse(str);
+//        Console.WriteLine($"Parsed number is {testint}");
+//    }
+//    catch (FormatException e)
+//    {
+//        Console.WriteLine($"Could not parse '{str}' : {e.Message}");
+//    }
+//}
+
+// Using TryParse method returns 'true' if the parse is successful
+result = int.TryParse(NumStr[0], out testint);
+Console.WriteLine($"{result} -- '{NumStr[0]}' : {testint}");
+
+result = float.TryParse(NumStr[1], out testfloat);
+Console.WriteLine($"{result} -- '{NumStr[1]}' : {testfloat}");
+
+result = int.TryParse(NumStr[2], out testint);
+Console.WriteLine($"{result} -- '{NumStr[2]}' : {testint}");
+
+result = float.TryParse(NumStr[3], out testfloat);
+Console.WriteLine($"{result} -- '{NumStr[3]}' : {testfloat}");
